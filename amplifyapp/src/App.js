f2d42593003c418aa4ef19b16935abb2d83e6c95
Mateusz Piwowarski <mateusz.piwowarski@euvic.pl>
2020-08-07 12:20:30 +0200

    display movie list

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movies from './components/movies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    fetch('https://li7tyqc3cj.execute-api.eu-central-1.amazonaws.com/movies/autocomplete?genres=1,3&phrase=the')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ movies: data })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Movies movies={this.state.movies} />
+    )
+  }
 }
 
 export default App;
---
+++ amplifyapp/src/components/movies.js
@@ -0,0 +1,23 @@
+import React from 'react'
+ 
+    const Genres = ['Horror', 'Adventure', 'Sci-fi', 'Thriller', 'Romance'];
+ 
+    const Movies = ({ movies }) => {
+      return (
+        <div>
+          <center><h1>Movies List</h1></center>
+          {movies.map((movies) => (
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{movies.Title}</h5>
+                 {movies.Genres.map((genres) =>
+                (<h6 class="card-subtitle mb-2 text-muted">{Genres[genres]}</h6>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    };
+ 
+    export default Movies
