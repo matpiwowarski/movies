e82e42e6a2ea961c9a766ff252b78a14519f3a93
Mateusz Piwowarski <mateusz.piwowarski@euvic.pl>
2020-08-10 13:13:18 +0200

    bootstrap template

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
 import SearchMovies from "./components/searchmovies";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   state = {
@@ -34,17 +35,40 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Search for movies</h1>
-        <div>
-          <SearchMovies
-            updatePhrase={this.updatePhrase}
-            updateGenres={this.updateGenres}
-          />
-        </div>
-        <button onClick={this.search}>Search</button>
-        <div>
-          <Movies movies={this.state.movies} />
+      <div className="container-fluid">
+        <div className="row no-gutter">
+          <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+          <div className="col-md-8 col-lg-6">
+            <div className="login d-flex align-items-center py-5">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-9 col-lg-8 mx-auto">
+                    <h3 className="login-heading mb-4">Welcome back!</h3>
+                    <form>
+                      <div className="form-label-group">// input email</div>
+
+                      <div className="form-label-group">// input password</div>
+
+                      <div className="custom-control custom-checkbox mb-3">
+                        // checkbox
+                      </div>
+                      <button
+                        className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
+                        type="submit"
+                      >
+                        Sign in
+                      </button>
+                      <div className="text-center">
+                        <a className="small" href="#">
+                          Forgot password?
+                        </a>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
